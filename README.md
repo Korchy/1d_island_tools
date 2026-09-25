0a56700cd85dfbf32a8241825fe20c95d7b0d3eb
Nikita Akimov <korchiy@yandex.ru>
2026-09-25 21:19:08 +0300

    update

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ Add-on functionality
 -
 Creates list of mesh islands for further working with them.
 
-Create Islands db - creates islands structure from active mesh.
+Create DB / Free DB - creates/releases islands structure from active mesh.
+
 Dec Selection - decrease selection, stay selected only whole selected islands.
-Select by Count - get number of vertices from whole selected islands and select all other non-selected islands with the same vertices amount.
+
+Islands by Verts Amount - get number of vertices from whole selected islands and select all other non-selected islands with the same vertices amount. Partially selected islands - deselects.
 
 Blender version
 -
--- island_tools.py
+++ island_tools.py
@@ -4,11 +4,11 @@
 # GitHub
 #    https://github.com/Korchy/1d_island_tools
 
+import time
 import bmesh
 import bpy
 from bpy.types import Operator, Panel
 from bpy.utils import register_class, unregister_class
-# import itertools
 
 bl_info = {
     "name": "Island Tools",
@@ -27,8 +27,65 @@ bl_info = {
 
 class IslandTools:
 
+    # islands_db - list of lists of vertices. Each list of vertices is an "island" - separated part of the mesh
+    islands_db = None   # static pointer to islands db
+    bm = None           # static BMesh object from which db was created
+
     @classmethod
-    def create_islands_db(cls, context):
+    def db(cls, obj, force_update=False):
+        # singleton for getting islands db
+        if (cls.islands_db is None) or force_update:
+            cls.update_db(obj=obj)
+        return cls.islands_db, cls.bm
+
+    @classmethod
+    def update_db(cls, obj):
+        # create/update islands db for object
+        start_time = time.time()
+        # create BMesh object
+        cls.bm = bmesh.new()
+        cls.bm.from_mesh(obj.data)
+        # cls.bm.verts.ensure_lookup_table()
+        # create islands db from BMesh object
+        cls.islands_db = []
+        checked = set() # already checked vertices
+        # check all vertices of the mesh
+        for vert in cls.bm.verts:
+            if vert in checked:
+                continue
+            checked.add(vert)
+            # current island
+            # island = []
+            vertices = []
+            edges = set()
+            faces = set()
+            # processing vertices
+            checking_vertices = [vert]
+            while checking_vertices:
+                current_vert = checking_vertices.pop()
+                vertices.append(current_vert)
+                # check vertices connected with current by edges
+                for edge in current_vert.link_edges:
+                    edges.add(edge)
+                    other = edge.other_vert(current_vert)
+                    if other not in checked:
+                        checked.add(other)
+                        checking_vertices.append(other)
+                    for face in edge.link_faces:
+                        faces.add(face)
+            # append new island to the islands db
+            cls.islands_db.append(
+                {
+                    'vertices': vertices,
+                    'edges': edges,
+                    'faces': faces
+                }
+            )
+        print('Islands DB (re)created in : ' + str(time.time() - start_time) \
+              + ' sec, got ' + str(len(cls.islands_db)) + ' islands.')
+
+    @classmethod
+    def update_islands_db(cls, context):
         # Create list of islands of active mesh
         src_obj = context.active_object
         # current mode
@@ -36,62 +93,89 @@ class IslandTools:
         # switch to OBJECT mode
         if src_obj.mode == 'EDIT':
             bpy.ops.object.mode_set(mode='OBJECT')
-        # bm object
-        bm = bmesh.new()
-        bm.from_mesh(src_obj.data)
-        bm.edges.ensure_lookup_table()
-        # create islands db
-
-        # save changed data to mesh
-        bm.to_mesh(src_obj.data)
-        bm.free()
+        # create/update islands db
+        cls.db(obj=src_obj, force_update=True)
         # return mode back
         context.scene.objects.active = src_obj
         bpy.ops.object.mode_set(mode=mode)
+
+    @classmethod
+    def free_islands_db(cls):
+        # free islands db
+        cls.islands_db = None
+        if cls.bm is not None:
+            cls.bm.free()
+            cls.bm = None
 
     @classmethod
     def dec_select(cls, context):
         # Decrease selection to whole selected islands
         src_obj = context.active_object
+        start_time = time.time()
         # current mode
         mode = src_obj.mode
         # switch to OBJECT mode
         if src_obj.mode == 'EDIT':
             bpy.ops.object.mode_set(mode='OBJECT')
-        # bm object
-        bm = bmesh.new()
-        bm.from_mesh(src_obj.data)
-        bm.edges.ensure_lookup_table()
+        # get islands db
+        db, bm = cls.db(obj=src_obj)
         # remove selection from partially selected islands
-
+        for island in db:
+            # if island is partially selected - remove selection
+            if any(not vertex.select for vertex in island['vertices']):
+                for vertex in island['vertices']:
+                    vertex.select = False
+                for edge in island['edges']:
+                    edge.select = False
+                for face in island['faces']:
+                    face.select = False
         # save changed data to mesh
         bm.to_mesh(src_obj.data)
-        bm.free()
         # return mode back
-        context.scene.objects.active = src_obj
         bpy.ops.object.mode_set(mode=mode)
+        print('Dec Select executed in : ' + str(time.time() - start_time) + ' sec.')
 
     @classmethod
     def islands_by_verts_amount(cls, context):
         # Select islands with the same vertices amount as already selected islands
         src_obj = context.active_object
+        start_time = time.time()
         # current mode
         mode = src_obj.mode
         # switch to OBJECT mode
         if src_obj.mode == 'EDIT':
             bpy.ops.object.mode_set(mode='OBJECT')
-        # bm object
-        bm = bmesh.new()
-        bm.from_mesh(src_obj.data)
-        bm.edges.ensure_lookup_table()
+        # get islands db
+        db, bm = cls.db(obj=src_obj)
         # select islands
-
+        vertices_amounts = set()
+        # collect amount of vertices for fully selected islands
+        for island in db:
+            # collect full selected islands by vertices amount
+            if all(vertex.select for vertex in island['vertices']):
+                vertices_amounts.add(len(island['vertices']))
+            # if island is partially selected - remove selection
+            if any(not vertex.select for vertex in island['vertices']):
+                for vertex in island['vertices']:
+                    vertex.select = False
+                for edge in island['edges']:
+                    edge.select = False
+                for face in island['faces']:
+                    face.select = False
+        # select islands with same amount of vertices
+        for island in db:
+            if len(island['vertices']) in vertices_amounts:
+                for vertex in island['vertices']:
+                    vertex.select = True
+                for edge in island['edges']:
+                    edge.select = True
+                for face in island['faces']:
+                    face.select = True
         # save changed data to mesh
         bm.to_mesh(src_obj.data)
-        bm.free()
         # return mode back
-        context.scene.objects.active = src_obj
         bpy.ops.object.mode_set(mode=mode)
+        print('Islands by Verts Amount executed in : ' + str(time.time() - start_time) + ' sec.')
 
     @staticmethod
     def _deselect_all(bm):
@@ -106,10 +190,17 @@ class IslandTools:
     @staticmethod
     def ui(layout):
         # ui panel
-        # Create Islands DB
-        layout.operator(
-            operator='island_tools.create_islands_db',
-            icon='SCENE_DATA'
+        # Create/Update/Free Islands DB
+        row = layout.row(align=True)
+        row.operator(
+            operator='island_tools.update_islands_db',
+            icon='SCENE_DATA',
+            text = 'Create DB'
+        )
+        row.operator(
+            operator='island_tools.free_islands_db',
+            icon='CANCEL',
+            text = 'Free DB'
         )
         # Dec Select
         layout.operator(
@@ -125,14 +216,14 @@ class IslandTools:
 
 # OPERATORS
 
-class IslandTools_OT_create_db(Operator):
-    bl_idname = 'island_tools.create_islands_db'
-    bl_label = 'Create Islands DB'
-    bl_description = 'Create islands DB'
+class IslandTools_OT_update_db(Operator):
+    bl_idname = 'island_tools.update_islands_db'
+    bl_label = 'Create/Update Islands DB'
+    bl_description = 'Create/Update islands DB'
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        IslandTools.create_islands_db(
+        IslandTools.update_islands_db(
             context=context
         )
         return {'FINISHED'}
@@ -140,6 +231,16 @@ class IslandTools_OT_create_db(Operator):
     @classmethod
     def poll(cls, context):
         return context.active_object and context.active_object.mode == 'EDIT'
+
+class IslandTools_OT_free_db(Operator):
+    bl_idname = 'island_tools.free_islands_db'
+    bl_label = 'Free Islands DB'
+    bl_description = 'Free islands DB'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        IslandTools.free_islands_db()
+        return {'FINISHED'}
 
 class IslandTools_OT_dec_select(Operator):
     bl_idname = 'island_tools.dec_select'
@@ -191,7 +292,8 @@ class IslandTools_PT_panel(Panel):
 # REGISTER
 
 def register(ui=True):
-    register_class(IslandTools_OT_create_db)
+    register_class(IslandTools_OT_update_db)
+    register_class(IslandTools_OT_free_db)
     register_class(IslandTools_OT_dec_select)
     register_class(IslandTools_OT_islands_by_verts_amount)
     if ui:
@@ -203,7 +305,8 @@ def unregister(ui=True):
         unregister_class(IslandTools_PT_panel)
     unregister_class(IslandTools_OT_islands_by_verts_amount)
     unregister_class(IslandTools_OT_dec_select)
-    unregister_class(IslandTools_OT_create_db)
+    unregister_class(IslandTools_OT_free_db)
+    unregister_class(IslandTools_OT_update_db)
 
 
 if __name__ == "__main__":
